a52a058ffffbf0418f45307fcc8849ad98192941
zhanshencanxing <34231561+zhanshencanxing@users.noreply.github.com>
2019-05-19 20:33:32 +0800

    音乐列表配置
    定义的音乐列表，只需写入名字，不能包含%等特殊字符

---
+++ musicList.js
@@ -0,0 +1,36 @@
+/*
+ * Browser	    MP3	    WAV  	OGG
+ * IE9+	        YES	    NO	    NO
+ * Chrome	    YES	    YES 	YES
+ * Firefox	    YES 	YES 	YES
+ * Safari	    YES	    YES 	NO
+ * Opera	    YES	    YES	    YES
+ * Edge         YES     YES     NO
+ * Wallpaper    NO      YES     YES
+ */
+var musicList = [
+	"主人(欢迎回来)-天降之物.mp3",
+    "1.MP3",
+    "Anastasia Kushnir - Yesterday [mqms].mp3",
+    "Fallen_Down__(《天降之物》插曲)-电视原声.mp3",
+    "ひぐらしのなく頃に-島みやえい子.mp3",
+    "笨蛋笨蛋-天降之物.mp3",
+    "狐妖小红娘动画 - 铭记 - 《狐妖小红娘》动画片头曲.mp3",
+    "回レ__雪月花-原田ひとみ_茅野愛衣_小倉唯.mp3",
+    "君临天下-洛天依_王朝-6726366.mp3",
+    "空想メソロギヰ-妖精帝國.mp3",
+    "藍井エイル (蓝井艾露) - MEMORIA (记忆) [mqms2].mp3",
+    "恋は渾沌の隷也-後ろから這いより隊G.mp3",
+    "洛天依 - 极乐净土 [mqms2].mp3",
+    "千本樱-洛天依-6441469.mp3",
+    "權御天下-洛天依-6309464.mp3",
+    "人间清欢-乐正绫_言和_洛天依-6726363.mp3",
+    "霜雪千年-乐正绫_洛天依-6685749.mp3",
+    "太陽曰く燃えよカオス-动漫原声.mp3",
+    "邪悪な都市-MONACA-6308056.mp3",
+    "楽園Project-Ray.mp3",
+    "澤野弘之&mizuki - aLIEz [mqms2].mp3",
+    "只有神知道的世界 - God Only Knows.mp3",
+];
+
+
